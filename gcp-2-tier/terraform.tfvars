1d4a658c6e367d0074f78a958e79166ca563ffc9
hayleyshim <yhshim17@gmail.com>
2022-02-22 15:55:46 +0900

    add webserver-variables

--- gcp-2-tier/terraform.tfvars
+++ gcp-2-tier/terraform.tfvars
@@ -1,7 +1,13 @@
 project_name = "vm-test-341412"
+name          = "vm-test" #추가
 region = "us-west2"
 zone = "us-west2-a"
 bucket_name = "tf_backend_gcp_banuka_jana_jayarathna_k8s"
 prefix = "terraform/gcp/boilerplate"
 token_path = "./token.json"
 storage_class = "REGIONAL"
+
+##################webserver 추가#########################
+
+backend-port  = "80" #추가
+frontend-port = "80" #추가
--- gcp-2-tier/variables.tf
+++ gcp-2-tier/variables.tf
@@ -25,3 +25,54 @@ variable "token_path" {
 variable "storage_class" {
   type = string
 }
+
+
+###########################################
+#                                         #
+#                webserver 추가            # 
+#                                         #
+###########################################
+
+variable "name" {
+  type = string
+}
+
+variable "network" {
+  type    = string
+  default = "default"
+}
+
+variable "vm-type" {
+  type    = string
+  default = "f1-micro"
+}
+
+variable "vm-startup-script" {
+  type    = string
+  default = "apt update && apt -y install apache2 && echo '<html><body><p>Linux startup script added directly.</p></body></html>' > /var/www/html/index.html"
+}
+
+variable "vm-image" {
+  type    = string
+  default = "debian-cloud/debian-9"
+}
+
+variable "backend-port" {
+  type = number
+}
+
+variable "frontend-port" {
+  type = number
+}
+
+variable "source-ranges" {
+  type    = list(string)
+  default = ["35.191.0.0/16", "130.211.0.0/22"]
+}
+
+variable "ip_protocol" {
+  type    = string
+  default = "tcp"
+}
+
+
